78139bd08f123ea5fe0a5e6e1b50243821e99b98
Juan Mangueira <juanmangueiraa@gmail.com>
2024-06-05 14:15:06 -0300

    Processamento: Trata colunas e agrupa dados das empresas em um único dataset

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -2,7 +2,7 @@ import json
 import csv
 
 def coleta_dados():
-    path = '../data_raw/'
+    path = './data_raw/'
     with open(path + 'dados_empresaA.json', 'r') as file:
         dados_empresaA = json.load(file)
     
@@ -16,6 +16,28 @@ def coleta_dados():
 
 def main():
     dados_empresaA, dados_empresaB = coleta_dados()
-    print(dados_empresaA)
-    print(dados_empresaB)
+    colunas_empresaA = list(dados_empresaA[0].keys())
+    colunas_empresaB = list(dados_empresaB[0].keys())
+    print(colunas_empresaA)
+    print(colunas_empresaB)
+
+    key_mapping = {'Nome do Item':'Nome do Produto', 
+                   'Classificação do Produto':'Categoria do Produto', 
+                   'Valor em Reais (R$)':'Preço do Produto (R$)', 
+                   'Quantidade em Estoque':'Quantidade em Estoque', 
+                   'Nome da Loja':'Filial', 
+                   'Data da Venda':'Data da Venda'}
+    
+    new_dados_empresaB = []
+    
+    for dict_old in dados_empresaB:
+        dict_temp = {}
+        for key_old, value in dict_old.items():
+            dict_temp[key_mapping[key_old]] = value
+        new_dados_empresaB.append(dict_temp)
+    
+    dados_empresas = []
+    dados_empresas.extend(dados_empresaA)
+    dados_empresas.extend(new_dados_empresaB)
+
 main()
